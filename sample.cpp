463dda244865e0c22f151f2f6f543aad620788d2
Jos Sebastian <sebastian.jos2222@gmail.com>
2024-10-13 18:56:07 -0500

    corrections

--- sample.cpp
+++ sample.cpp
@@ -8,18 +8,32 @@ using std::cout, std::endl;
 #include <random>
 using std::random_device, std::mt19937, std::uniform_int_distribution;
 #include <algorithm>
-using std::swap, std::shuffle, std::reverse, std::merge, std::copy;
+using std::sort, std::is_sorted;
+using std::swap, std::shuffle, std::reverse;
 
 #include <vector>
 using std::vector;
+#include <string>
+using std::string;
 
 #include <mpi.h>
 
 #define MASTER 0
 
-void Configure(int size, int type, vector<int> &array)
+void Start(int process, int processes, int size, int type, int &subsize, int &subtype, vector<int> &subarray)
 {
-    array.resize(size);
+    if (process == MASTER)
+        cout << "Sample Sort: "
+             << "Size" << "(" << size << ")"
+             << " - "
+             << "Type" << "(" << type << ")"
+             << endl;
+
+    subsize = size / processes;
+    subtype = type;
+    subarray.resize(subsize);
+
+    vector<int> array(size);
     for (int index = 0; index < size; index++)
     {
         array.at(index) = index;
@@ -32,7 +46,7 @@ void Configure(int size, int type, vector<int> &array)
         uniform_int_distribution<int> distribution(0, size - 1);
 
         int perturb = size / 100;
-        for (int i = 0; i < perturb / 2; ++i)
+        for (int i = 0; i < perturb; ++i)
         {
             int index1 = distribution(generator);
             int index2 = distribution(generator);
@@ -45,71 +59,105 @@ void Configure(int size, int type, vector<int> &array)
         mt19937 generator(device());
         shuffle(array.begin(), array.end(), generator);
     }
-    if (type == 2)
+    if (type == 3)
     {
         reverse(array.begin(), array.end());
     }
+
+    MPI_Scatter(array.data(), subsize, MPI_INT, subarray.data(), subsize, MPI_INT, MASTER, MPI_COMM_WORLD);
 }
 
-void Check(int size, int type, vector<int> &array)
+void End(int process, int processes, int size, int type, int &subsize, int &subtype, std::vector<int> &subarray)
 {
-    bool sort = true;
-    for (int index = 0; index < size - 1; ++index)
+    bool sort = is_sorted(subarray.begin(), subarray.end());
+
+    if (sort)
     {
-        if (array.at(index) > array.at(index + 1))
+        MPI_Status status;
+        int first, last;
+        int before, after;
+
+        if (process == 0)
         {
-            sort = false;
-            break;
+            last = subarray.at(subarray.size() - 1);
+
+            MPI_Send(&last, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+            MPI_Recv(&after, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
+
+            if (last > after)
+                sort = false;
+        }
+        else if (process == processes - 1)
+        {
+            first = subarray.at(0);
+
+            MPI_Send(&first, 1, MPI_INT, processes - 2, 0, MPI_COMM_WORLD);
+            MPI_Recv(&before, 1, MPI_INT, processes - 2, 0, MPI_COMM_WORLD, &status);
+
+            if (first < before)
+                sort = false;
+        }
+        else
+        {
+            first = subarray.at(0);
+            last = subarray.at(subarray.size() - 1);
+
+            MPI_Send(&first, 1, MPI_INT, process - 1, 0, MPI_COMM_WORLD);
+            MPI_Send(&last, 1, MPI_INT, process + 1, 0, MPI_COMM_WORLD);
+            MPI_Recv(&before, 1, MPI_INT, process - 1, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv(&after, 1, MPI_INT, process + 1, 0, MPI_COMM_WORLD, &status);
+
+            if (first < before || last > after)
+                sort = false;
         }
     }
 
-    cout << "Sample Sort: " << (sort ? "Success" : "Failure") << endl;
+    bool status = false;
+    MPI_Reduce(&sort, &status, 1, MPI_C_BOOL, MPI_LAND, MASTER, MPI_COMM_WORLD);
+
+    if (process == MASTER)
+        cout << "Sample Sort: "
+             << (status ? "Success" : "Failure")
+             << endl;
 }
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 3)
+    int size, type;
     {
-        cout << "Usage: ./sample.cpp <size> <type>" << endl;
-        return 1;
-    }
+        if (argc != 3)
+        {
+            cout << "Usage: ./sample.cpp <size> <type>" << endl;
+            return 1;
+        }
 
-    int size = static_cast<int>(pow(2, atoi(argv[1])));
+        int s = atoi(argv[1]);
+        if (s <= 0)
+        {
+            cout << "ERROR: Array Size" << endl;
+            return 1;
+        }
+        size = static_cast<int>(pow(2, s));
 
-    int type = atoi(argv[2]);
-    char *types[] = {"sort", "perturb", "random", "reverse"};
-
-    if (size <= 0)
-    {
-        cout << "ERROR: Array Size" << endl;
-        return 1;
-    }
-    if (type < 0 || type > 3)
-    {
-        cout << "ERROR: Array Type" << endl;
-        return 1;
+        int t = atoi(argv[2]);
+        if (t < 0 || t > 3)
+        {
+            cout << "ERROR: Array Type" << endl;
+            return 1;
+        }
+        type = static_cast<int>(t);
     }
 
     int processes, process;
     MPI_Init(&argc, (char ***)&argv);
     MPI_Comm_size(MPI_COMM_WORLD, &processes);
     MPI_Comm_rank(MPI_COMM_WORLD, &process);
+    MPI_Status status;
 
-    if (process == MASTER)
-    {
-        cout << "Sample Sort: " << "Size(" << size << ") - Type(" << types[type] << ")" << endl;
-    }
+    int subsize, subtype;
+    vector<int> subarray;
 
-    vector<int> array;
-    if (process == MASTER)
-    {
-        Configure(size, type, array);
-    }
-
-    int subsize = size / processes;
-    vector<int> subarray(subsize);
-
-    MPI_Scatter(array.data(), subsize, MPI_INT, subarray.data(), subsize, MPI_INT, MASTER, MPI_COMM_WORLD);
+    Start(process, processes, size, type, subsize, subtype, subarray);
 
     sort(subarray.begin(), subarray.end());
 
@@ -120,9 +168,7 @@ int main(int argc, char const *argv[])
     }
 
     vector<int> samples(processes * (processes - 1));
-
     MPI_Gather(candidates.data(), processes - 1, MPI_INT, samples.data(), processes - 1, MPI_INT, MASTER, MPI_COMM_WORLD);
-
     sort(samples.begin(), samples.end());
 
     vector<int> splitters(processes - 1);
@@ -130,7 +176,6 @@ int main(int argc, char const *argv[])
     {
         splitters.at(index) = samples.at((index + 1) * (processes - 1));
     }
-
     MPI_Bcast(splitters.data(), processes - 1, MPI_INT, MASTER, MPI_COMM_WORLD);
 
     vector<vector<int>> buckets(processes);
@@ -152,57 +197,35 @@ int main(int argc, char const *argv[])
         }
     }
 
-    int partner;
-    bool complete = false;
-    int bitmask = 1;
-    MPI_Status status;
+    subarray = buckets.at(process);
 
-    while (!complete && bitmask < processes)
+    for (int index = 0; index < processes; index++)
     {
-        partner = process ^ bitmask;
-        if (process < partner)
-        {
-            for (int index = 0; index < processes; index++)
-            {
-                int bucket;
-                MPI_Recv(&bucket, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, &status);
-
-                vector<int> temporary(bucket);
-                MPI_Recv(temporary.data(), bucket, MPI_INT, partner, 0, MPI_COMM_WORLD, &status);
-
-                vector<int> result(buckets.at(index).size() + temporary.size());
-                merge(buckets.at(index).begin(), buckets.at(index).end(), temporary.begin(), temporary.end(), result.begin());
-                buckets.at(index) = result;
-            }
-            bitmask <<= 1;
-        }
-        else
-        {
-            for (int index = 0; index < processes; index++)
-            {
-                int bucket = static_cast<int>(buckets.at(index).size());
-                MPI_Send(&bucket, 1, MPI_INT, partner, 0, MPI_COMM_WORLD);
-                MPI_Send(buckets.at(index).data(), bucket, MPI_INT, partner, 0, MPI_COMM_WORLD);
-            }
-            complete = true;
-        }
-    }
-
-    if (process == MASTER)
-    {
-        int position = 0;
-        for (int index = 0; index < processes; index++)
+        if (process != index)
         {
             int bucket = static_cast<int>(buckets.at(index).size());
-            copy(buckets.at(index).begin(), buckets.at(index).end(), array.begin() + position);
-            position += bucket;
+            MPI_Send(&bucket, 1, MPI_INT, index, 0, MPI_COMM_WORLD);
+            MPI_Send(buckets.at(index).data(), bucket, MPI_INT, index, 0, MPI_COMM_WORLD);
         }
     }
 
-    if (process == MASTER)
+    for (int index = 0; index < processes; index++)
     {
-        Check(size, type, array);
+        if (process != index)
+        {
+            int bucket;
+            MPI_Recv(&bucket, 1, MPI_INT, index, 0, MPI_COMM_WORLD, &status);
+
+            vector<int> temporary(bucket);
+            MPI_Recv(temporary.data(), bucket, MPI_INT, index, 0, MPI_COMM_WORLD, &status);
+
+            vector<int> result(subarray.size() + temporary.size());
+            merge(subarray.begin(), subarray.end(), temporary.begin(), temporary.end(), result.begin());
+            subarray = result;
+        }
     }
+
+    End(process, processes, size, type, subsize, subtype, subarray);
 
     MPI_Finalize();
 }
